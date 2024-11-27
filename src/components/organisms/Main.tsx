a2632ddf689d9e25fe35f6469fccf9bc8fb35102
lonchop <goranjose16@gmail.com>
2024-11-27 07:52:04 -0400

    Install Next.js - App router
    Add config Prettier and Eslint
    
    Install Tailwind Automatic Class Sorting with Prettier
    
    Install React Icons
    
    Create Atomic design

---
+++ src/components/organisms/Main.tsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1>Calcoin</h1>
+    </main>
+  )
+}
---
+++ src/config/fonts.ts
@@ -0,0 +1,10 @@
+import { Poppins, Inter } from 'next/font/google'
+
+export const poppins = Poppins({ 
+  subsets: ['latin'], 
+  weight: ['400', '600', '700'],
+ })
+
+export const inter = Inter({ 
+  subsets: ['latin'] 
+})
